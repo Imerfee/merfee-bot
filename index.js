847bc499474aca31764bd12ea410859c1fbc1599
Imerfee <imerfee@gmail.com>
2025-04-09 20:44:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
 
+// Получаем токен бота из переменной окружения
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Приветственное сообщение при старте
+bot.start((ctx) => {
+  ctx.reply('Привет! Я готов к обмену валют. Напиши /help для справки.');
+});
+
+// Справка о боте
+bot.help((ctx) => {
+  ctx.reply('Этот бот поможет вам обменять валюту. Просто выберите нужные валюты.');
+});
+
+// Запуск бота
+bot.launch();
