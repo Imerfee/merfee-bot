17681104f1c2d1d19e6f7d5e59b88a80b65fa14d
Imerfee <imerfee@gmail.com>
2025-04-09 15:55:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
-import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.types import ParseMode
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
+from flask import Flask
 
-# Ваш токен от бота Telegram
-TOKEN = '7426766382:AAG-Fw82VsIKowP_c3zVEoaVQQoa_LHWXeU'  # Замените на ваш
+API_TOKEN = '7426766382:AAG-Fw82VsIKowP_c3zVEoaVQQoa_LHWXeU'  # Укажите ваш токен
 
-# Инициализация бота и диспетчера
-bot = Bot(token=TOKEN)
+bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
+app = Flask(__name__)
 
-# Приветствие и меню выбора языка
+# Стартовое сообщение бота
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
+    await message.answer("Привет! Выберите язык. /choose_language")
+
+# Обработчик команды выбора языка
+@dp.message_handler(commands=['choose_language'])
+async def cmd_choose_language(message: types.Message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("English")
-    btn2 = types.KeyboardButton("Русский")
-    btn3 = types.KeyboardButton("Deutsch")
-    btn4 = types.KeyboardButton("Français")
-    markup.add(btn1, btn2, btn3, btn4)
-    await message.answer("Welcome to Merfee Exchange! Please choose your language:", reply_markup=markup)
+    markup.add("English", "Русский")
+    await message.answer("Выберите язык:", reply_markup=markup)
 
-# Обработка выбора языка
-@dp.message_handler(lambda message: message.text in ['English', 'Русский', 'Deutsch', 'Français'])
-async def choose_language(message: types.Message):
-    language = message.text
-    await message.answer(f'You have selected: {language}', reply_markup=types.ReplyKeyboardRemove())
+# Логика выбора языка
+@dp.message_handler(lambda message: message.text in ["English", "Русский"])
+async def language_choice(message: types.Message):
+    if message.text == "English":
+        await message.answer("You selected English.")
+    elif message.text == "Русский":
+        await message.answer("Вы выбрали русский.")
 
-# Запуск бота
+# Вебхук для обработки запросов (не обязательно, если вы хотите использовать поллинг)
+@app.route('/' + API_TOKEN, methods=["POST"])
+async def handle_webhook(request):
+    json_str = await request.get_data(as_text=True)
+    update = types.Update.de_json(json_str)
+    await dp.process_update(update)
+    return "OK", 200
+
 if __name__ == '__main__':
+    # Запуск бота
+    from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
